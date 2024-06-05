9cef8fff456e3ac14c07d63c4fbbf97e105a3a69
jhair <jinfanzonquispe30@gmail.com>
2024-06-05 01:42:25 -0500

    add menu

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -7,3 +7,13 @@
     }
 
     add_action( 'init','gymfitness_menus' );
+
+
+    /* CARGAR  */
+
+    function gymfitness_scripts_styles() {
+        wp_enqueue_style( 'normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(),'8.0.1' );
+        wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'),'1.0.0' );
+    }
+
+    add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     <header class="header">
@@ -20,7 +21,6 @@
 
             ?>
             
-        
             
         </div>
 
